7ae32ad6d927f5f643d8079ce775e317034dc6c6
Juan Alvarez <jmalvarezd@unal.edu.co>
2018-03-05 22:40:09 -0500

    Cambios de Laiton

--- src/AlgoritmosDeBusqueda/CuadroTablero.java
+++ src/AlgoritmosDeBusqueda/CuadroTablero.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
  * @author Usuario
  */
 public class CuadroTablero implements Serializable {
-
     public static final int CUADRO1 = 1;
     public static final int CUADRO2 = 2;
     public static final int CUADRO3 = 3;
@@ -29,60 +28,47 @@ public class CuadroTablero implements Serializable {
     public static final int CUADRO14 = 14;
     public static final int CUADRO15 = 15;
     public static final int VACIO = 0;
-
+    
     public int[][] data;
-
+    
     public CuadroTablero() {
-        this.data = new int[4][4];
-        this.data[0][0] = CuadroTablero.CUADRO1;
-        this.data[0][1] = CuadroTablero.CUADRO2;
-        this.data[0][2] = CuadroTablero.CUADRO3;
-        this.data[0][3] = CuadroTablero.CUADRO4;
-        this.data[1][0] = CuadroTablero.CUADRO5;
-        this.data[1][1] = CuadroTablero.CUADRO6;
-        this.data[1][2] = CuadroTablero.CUADRO7;
-        this.data[1][3] = CuadroTablero.CUADRO8;
-        this.data[2][0] = CuadroTablero.CUADRO9;
-        this.data[2][1] = CuadroTablero.CUADRO10;
-        this.data[2][2] = CuadroTablero.CUADRO11;
-        this.data[2][3] = CuadroTablero.CUADRO12;
-        this.data[3][0] = CuadroTablero.CUADRO13;
-        //MODIFICAR
-        this.data[3][1] = CuadroTablero.CUADRO14;
-        this.data[3][2] = CuadroTablero.VACIO;
-        this.data[3][3] = CuadroTablero.CUADRO15;
+    	this.data = new int[4][4];
+	this.data[0][0] = CuadroTablero.CUADRO1;
+    	this.data[0][1] = CuadroTablero.CUADRO2;
+    	this.data[0][2] = CuadroTablero.CUADRO3;
+    	this.data[0][3] = CuadroTablero.CUADRO4;
+    	this.data[1][0] = CuadroTablero.CUADRO5;
+    	this.data[1][1] = CuadroTablero.CUADRO6;
+    	this.data[1][2] = CuadroTablero.CUADRO7;
+    	this.data[1][3] = CuadroTablero.CUADRO8;
+    	this.data[2][0] = CuadroTablero.CUADRO9;
+    	this.data[2][1] = CuadroTablero.CUADRO10;
+    	this.data[2][2] = CuadroTablero.CUADRO11;
+    	this.data[2][3] = CuadroTablero.CUADRO12;
+    	this.data[3][0] = CuadroTablero.CUADRO13;
+    	this.data[3][1] = CuadroTablero.CUADRO14;
+    	this.data[3][2] = CuadroTablero.CUADRO15;
+    	this.data[3][3] = CuadroTablero.VACIO;
 
     }
 
-    public CuadroTablero(int[][] data) {
-        this.data = data;
-    }
-
-    public void set(int x, int y, int i) {
-        this.data[x][y] = i;
-    }
-
-    public int get(int x, int y) {
-        return this.data[x][y];
-    }
-
-    public int[][] getAll() {
-        return this.data;
-    }
-
-    public int rows() {
-        return data.length;
-    }
-
-    public int columns() {
-        return data[0].length;
-    }
-
-    public void printTablero() {
+    public CuadroTablero( int[][] data ){ this.data = data; }
+    
+    public void set( int x, int y, int i ){ this.data[x][y]=i; }
+    
+    public int get(int x, int y){ return this.data[x][y]; }
+    
+    public int[][] getAll(){return this.data;}
+    
+    public int rows(){ return data.length; }
+    public int columns(){ return data[0].length; }
+    
+    public void printTablero(){
         System.out.println(this.data[0][0] + " " + this.data[0][1] + " " + this.data[0][2] + " " + this.data[0][3] + " ");
         System.out.println(this.data[1][0] + " " + this.data[1][1] + " " + this.data[1][2] + " " + this.data[1][3] + " ");
         System.out.println(this.data[2][0] + " " + this.data[2][1] + " " + this.data[2][2] + " " + this.data[2][3] + " ");
         System.out.println(this.data[3][0] + " " + this.data[3][1] + " " + this.data[3][2] + " " + this.data[3][3] + " ");
     }
-
+    
 }
+
--- src/AlgoritmosDeBusqueda/Main.java
+++ src/AlgoritmosDeBusqueda/Main.java
@@ -10,33 +10,35 @@ package AlgoritmosDeBusqueda;
  * @author Usuario
  */
 public class Main {
-
     public static void main(String[] args) {
         Intercambio cambio = new Intercambio();
         CuadroSucesor sucesorCuadro = new CuadroSucesor();
         CuadroObjetivo objetivoCuadro = new CuadroObjetivo();
-//        BFS buscadorAmplitud = new BFS(sucesorCuadro, objetivoCuadro);
-//        DFS buscadorProfundidad = new DFS(sucesorCuadro,objetivoCuadro);
-        //       DFSLimitado buscadorProfundidadLimitada = new DFSLimitado(sucesorCuadro,objetivoCuadro,5);
-        LimiteIterado buscadorProfundidadIterada = new LimiteIterado(sucesorCuadro,objetivoCuadro);
-//        CostoUniforme buscadorCostoUniforme = new CostoUniforme(sucesorCuadro, objetivoCuadro);
-//        Astar buscadorAstar = new Astar(sucesorCuadro, objetivoCuadro, new HeuristicaRabon());
-        CuadroTablero cuadroInicial = new CuadroTablero();
-//        cuadroInicial.data = cambio.desordenarMatriz(cuadroInicial.data); //ESTO DESORDENA, SOLO COMENTAR
-        System.out.println("Tablero inicial:");
-        cuadroInicial.printTablero();
-//        Arco<CuadroTablero> encontrado = buscadorAmplitud.aplicar(cuadroInicial);
-//        Arco<CuadroTablero> encontrado = buscadorProfundidad.aplicar(cuadroInicial);
-        //      Arco<CuadroTablero> encontrado = buscadorProfundidadLimitada.aplicar(cuadroInicial);     
-       Arco<CuadroTablero> encontrado = buscadorProfundidadIterada.aplicar(cuadroInicial);
-//        Arco<CuadroTablero> encontrado = buscadorCostoUniforme.aplicar(cuadroInicial);
-//        Arco<CuadroTablero> encontrado = buscadorAstar.aplicar(cuadroInicial);
-        if (encontrado != null) {
+   //    BFS buscadorAmplitud = new BFS(sucesorCuadro,objetivoCuadro);
+        //DFS buscadorProfundidad = new DFS(sucesorCuadro,objetivoCuadro);
+      // DFSLimitado buscadorProfundidadLimitada = new DFSLimitado(sucesorCuadro,objetivoCuadro,15);
+     //  LimiteIterado buscadorProfundidadIterada = new LimiteIterado(sucesorCuadro,objetivoCuadro);
+      // CostoUniforme buscadorCostoUniforme = new CostoUniforme(sucesorCuadro, objetivoCuadro);
+      // Astar buscadorAstar = new Astar(sucesorCuadro, objetivoCuadro, new HeuristicaRabon());
+      Astar buscadorAstar = new Astar(sucesorCuadro, objetivoCuadro, new HeuristicaManhattan());  
+      CuadroTablero cuadroInicial = new CuadroTablero();
+      cuadroInicial.data = cambio.desordenarMatriz(cuadroInicial.data); //ESTO DESORDENA, SOLO COMENTAR
+        System.out.println("Tablero inicial:");  
+      cuadroInicial.printTablero();            
+       //  Arco<CuadroTablero> encontrado = buscadorAmplitud.aplicar(cuadroInicial);
+      // Arco<CuadroTablero> encontrado = buscadorProfundidad.aplicar(cuadroInicial);
+     // Arco<CuadroTablero> encontrado = buscadorProfundidadLimitada.aplicar(cuadroInicial);     
+     //  Arco<CuadroTablero> encontrado = buscadorProfundidadIterada.aplicar(cuadroInicial);
+      // Arco<CuadroTablero> encontrado = buscadorCostoUniforme.aplicar(cuadroInicial);
+      // Arco<CuadroTablero> encontrado = buscadorAstar.aplicar(cuadroInicial);
+      Arco<CuadroTablero> encontrado = buscadorAstar.aplicar(cuadroInicial);
+        if(encontrado!=null){
             CuadroTablero tableroEncontrado = encontrado.e;
             System.out.println("tablero encontrado:");
             tableroEncontrado.printTablero();
-            System.out.println("En " + encontrado.getAccionAcumulada() + " pasos");
-        } else {
+            System.out.println("En " + encontrado.getAccionAcumulada()+ " pasos");  
+            System.out.println("el maximo es " + buscadorAstar.max);  
+        }else{
             System.out.println("No encontrado");
         }
 
